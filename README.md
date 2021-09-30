48af9ab84afdd23367477fa5fd36dfbeb2b8ae7d
Lewin <junxianalan@gmail.com>
2021-09-30 14:11:28 +0800

    init-待实现功能

---
+++ README.md
@@ -0,0 +1,15 @@
+# rreact-native-system-file-browser
+react-native 识别照片二维码
+
+## 目录
+- [安装](#install)
+- [使用](#usage)
+
+## Install
+### 1: yarn add 或者npm install,现在最新版本是1.1.0
+`yarn add rreact-native-system-file-browser  `
+### 2: yarn install 或 npm install
+### 3: cd ios && pod install
+
+## Usage
+### NOTE: 可以参考demo的App.js中的
---
+++ demo/App.js
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  ActionSheetIOS,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+// import OpenMapNavigation from './native'
+
+import OpenMapNavigation from 'react-native-open-map-navigation'
+
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({ children, title }): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <TouchableOpacity style={styles.btn} onPress={() => {
+            OpenMapNavigation.openMapActionSheet("121.467237", "31.234532", "上海华盛大公馆", {
+              onFail: (e) => {
+                console.log(e)
+              },
+              onNoMapApp: () => {
+                console.log("没有相关地图")
+              },
+              onSelectItem: (item) => {
+                console.log("选择了：%o", item)
+              }
+            })
+          }}>
+            <Text style={{ color: 'white' }}>打开自带地图</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  btn: {
+    backgroundColor: 'blue',
+    padding: 15,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default App;
